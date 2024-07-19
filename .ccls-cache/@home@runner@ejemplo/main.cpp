d4a727418ceadcf03526e0f31f0090134ec29689
Java3243 <guillermoruiz@micorreo.upp.edu.mx>
2024-07-19 20:32:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@ejemplo/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+
+  float celsius,farenheit;
+  cout<<"ingrese la temperatura en grados celsius:";
+  cin>>celsius;
+  farenheit=9/5.0*celsius+32;
+  cout<<"los grados a farenheit son:"<<farenheit;
+
+}
